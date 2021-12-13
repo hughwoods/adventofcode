bc8e96a99790f047d0b09d87128ebd3bf5d7ccab
hughwoods <3420970+hughwoods@users.noreply.github.com>
2021-12-13 14:54:44 +0000

    refactor day 10

--- src/main/scala/twentytwentyone/Day10.scala
+++ src/main/scala/twentytwentyone/Day10.scala
@@ -4,6 +4,7 @@ import cats.parse.Parser.Error
 import cats.parse.{Parser, Parser0}
 
 import scala.Function.tupled
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Day10 {
@@ -11,57 +12,51 @@ object Day10 {
   val input = Source.fromResource("2021/input_day_10.txt").getLines().toSeq
   val chunkDefinitions: List[(Char, Char)] = List(('<', '>'), ('[', ']'), ('{', '}'), ('(', ')'))
 
-  def result1(input: Seq[String]): Long = input
-    .map(s => (s, Day10.parsers.chunks.parse(s)))
-    .collect { case (s, Left(Error(offset, _))) => s(offset) }
-    .map {
-      case ')' => 3
-      case ']' => 57
-      case '}' => 1197
-      case '>' => 25137
-    }
-    .sum
-
-  def result2(input: Seq[String]): Long = {
-    val completionScores = input
-      .map(Day10.parsers.chunks.parse)
-      .collect { case Right(("", chunks)) => chunks._2 }
-      .filter(_.isDefined)
-      .map(complete)
-      .map(scoreCompletion)
-
-    val index = completionScores.length / 2
-    completionScores.sorted.apply(index)
-  }
-
-  def scoreCompletion(com: String): Long = {
-    def scoreCompletionAcc(com: Seq[Char], acc: Long): Long =
-      com match {
-        case Nil    => acc
-        case h :: t => scoreCompletionAcc(t, (acc * 5) + scoreChar(h))
-      }
-    scoreCompletionAcc(com.toList, 0)
-  }
-
-  def scoreChar(c: Char): Int = c match {
-    case ')' => 1
-    case ']' => 2
-    case '}' => 3
-    case '>' => 4
-  }
-
-  def complete(oc: Option[OpenChunk]) = completeContinue(oc, "")
-
-  def completeContinue(oc: Option[OpenChunk], acc: String): String =
-    oc match {
-      case None        => acc
-      case Some(chunk) => completeContinue(chunk.tail, chunk.t + acc)
-    }
-
   def main(args: Array[String]): Unit = {
     println(s"part1: ${result1(input)}")
     println(s"part2: ${result2(input)}")
   }
+
+  def result1(input: Seq[String]): Long = input
+    .map(s => (s, Day10.parsers.chunks.parse(s)))
+    .collect { case (s, Left(Error(offset, _))) => s(offset) }
+    .map(autoCompleteScore)
+    .sum
+
+  def autoCompleteScore = Map(')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137)
+
+  def result2(input: Seq[String]): Long =
+    midpointValue(
+      input
+        .map(Day10.parsers.chunks.parse)
+        .collect { case Right(("", chunks)) if chunks._2.isDefined => chunks._2 }
+        .map(complete)
+        .map(scoreCompletion)
+    )
+
+  def midpointValue[A](l: Seq[A])(implicit ord: Ordering[A]): A = l.sorted.apply(l.length / 2)
+
+  def complete(oc: Option[OpenChunk]) = {
+    @tailrec
+    def completeAcc(oc: Option[OpenChunk], acc: String): String =
+      oc match {
+        case None        => acc
+        case Some(chunk) => completeAcc(chunk.tail, chunk.t + acc)
+      }
+    completeAcc(oc, "")
+  }
+
+  def scoreCompletion(com: String): Long = {
+    @tailrec
+    def scoreCompletionAcc(com: Seq[Char], acc: Long): Long =
+      com match {
+        case Nil     => acc
+        case c :: cs => scoreCompletionAcc(cs, (acc * 5) + charScore(c))
+      }
+    scoreCompletionAcc(com.toList, 0)
+  }
+
+  def charScore = Map(')' -> 1, ']' -> 2, '}' -> 3, '>' -> 4)
 
   case class Chunk(s: Char, t: Char, content: Seq[Chunk])
 
